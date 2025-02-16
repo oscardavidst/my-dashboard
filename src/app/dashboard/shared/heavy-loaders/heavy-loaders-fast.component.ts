c5ddb4c3e4b98a556bea8208ccf0a1a4a7940b48
Oscar Soto <oscarqpzm95@gmail.com>
2025-02-16 00:13:02 -0500

    Estructura del proyecto

---
+++ src/app/dashboard/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-fast',
+  imports: [],
+  template: `<h1>Hola heavy loaders</h1>`,
+})
+export class HeavyLoadersFastComponent {}
---
+++ src/app/dashboard/shared/heavy-loaders/users-loaders.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  template: `<h1>Hola user loader</h1>`,
+})
+export class NameComponent {
+  constructor() {}
+}
