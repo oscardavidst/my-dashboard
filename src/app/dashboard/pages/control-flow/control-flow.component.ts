c8d91d8f37fb030bc1ee13c59432b4ff09ab9868
Oscar Soto <oscarqpzm95@gmail.com>
2025-02-27 21:50:10 -0500

    updates

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+type Grade = 'A' | 'B' | 'D';
 
 @Component({
   imports: [],
   templateUrl: './control-flow.component.html',
   styles: ``,
 })
-export default class ControlFlowComponent {}
+export default class ControlFlowComponent {
+  public showContent = signal(false);
+  public grade = signal<Grade>('B');
+  public frameworks = signal<string[]>([
+    'Angular',
+    'React',
+    'Vue',
+    'Svelte',
+    'Ember',
+  ]);
+
+  public toogleContent() {
+    this.showContent.update((value) => !value);
+  }
+}
--- src/app/dashboard/pages/dashboard.component.ts
+++ src/app/dashboard/pages/dashboard.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { routes } from '../../app.routes';
 
 @Component({
   imports: [RouterModule],
   templateUrl: './dashboard.component.html',
   styles: ``,
 })
-export default class DashboardComponent {}
+export default class DashboardComponent {
+  public itemsMenu = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path && !route.path.includes(':'));
+
+  constructor() {}
+}
